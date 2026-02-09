b1c7769582dfbfb64e194c70bfe4126c6fe20a88
mithil <mithilsalunkhe@outlook.com>
2026-02-09 14:46:53 -0600

    Add configuration files and update project structure

---
+++ commands.sh
@@ -0,0 +1,6 @@
+srun -A betw-dtai-gh --time=01:00:00 --nodes=1 --ntasks-per-node=16 \
+--partition=ghx4-interactive --gpus=1 --mem=16g --pty /bin/bash
+
+apptainer run --nv \
+    --bind /projects \
+    /sw/user/NGC_containers/pytorch_25.08-py3.sif
