dcb9fa53d1e1d7c3e7e6ce587f2ea88ef7e01ca2
mithil <mithilsalunkhe@outlook.com>
2026-02-10 21:18:42 -0600

    new sbatch

--- scripts/DiT_sample.sh
+++ scripts/DiT_sample.sh
@@ -2,5 +2,5 @@ module load python/3.11.9
 module load cuda/12.6.1
 
 source ./venv/bin/activate
-
-torchrun --nnodes=1 --nproc_per_node=N sample_ddp.py --model DiT-XL/2 --num-fid-samples 50000
+cd /u/msalunkhe/DiffusionFusion/DiT
+torchrun --nnodes=1 --nproc_per_node=auto sample_ddp.py --model DiT-XL/2 --num-fid-samples 50000  --sample-dir /projects/betw/msalunkhe/DiT_generations
