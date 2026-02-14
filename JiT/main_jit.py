6e201ad7c68ea7d3840bb93307112ee49d9c0b6b
mithil <mithilsalunkhe@outlook.com>
2026-02-13 21:36:01 -0600

    update

--- JiT/main_jit.py
+++ JiT/main_jit.py
@@ -151,8 +151,9 @@ def main(args):
     
     ##dataset_train = dataset_train.shuffle(buffer_size=50_000, seed=0)
     ##dataset_train = dataset_train.shard(num_shards=num_tasks, index=global_rank)
-    dataset_train.set_transform(transform)
+    
     dataset_train = dataset_train.with_format("torch")
+    dataset_train = dataset_train.with_transform(transform)
     ##assert isinstance(dataset_train, torch.utils.data.IterableDataset)
 
 
